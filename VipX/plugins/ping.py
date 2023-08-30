93e19bea80e6f6a18e18af418fe592ddbe8b01bc
MISHSINGH19 <141798577+MISHSINGH19@users.noreply.github.com>
2023-08-30 07:29:57 +0530

    Update ping.py

--- VipX/plugins/ping.py
+++ VipX/plugins/ping.py
@@ -80,10 +80,10 @@ async def ping_com(client, message: Message, _):
         ],
         [
             InlineKeyboardButton(
-                text="♦️𝐆𝐑𝐎𝐔𝐏♦️", url=f"https://t.me/tg_friendss",
+                text="♦️𝐆𝐑𝐎𝐔𝐏♦️", url=f"https://t.me/HINDI_FRIENDS_CHATTING_ROOM",
             ),
             InlineKeyboardButton(
-                text="♦️𝐌𝐎𝐑𝐄♦️", url=f"https://t.me/vip_creators",
+                text="♦️𝐌𝐎𝐑𝐄♦️", url=f"https://t.me/HINDI_FRIENDS_CHATTING_ROOM",
             )
         ],
         [
