02d93010c40eb0995f66c1d855cd72fbb09386ce
rocks143014 <119806738+rocks143014@users.noreply.github.com>
2023-09-08 21:10:37 +0530

    Update telegraph.py

--- VipX/plugins/telegraph.py
+++ VipX/plugins/telegraph.py
@@ -7,7 +7,7 @@ from VipX import app
 def ul(_, message):
     reply = message.reply_to_message
     if reply.media:
-        i = message.reply("🌹𝐌𝐚𝐤𝐢𝐧𝐠 𝐀 𝐋𝐢𝐧𝐤 𝐎𝐟 𝐘𝐨𝐮𝐫 𝐃𝐨𝐜𝐮𝐦𝐞𝐧𝐭 𝐁𝐚𝐛𝐲....🌹")
+        i = message.reply("𝐌𝐚𝐤𝐢𝐧𝐠 𝐀 𝐋𝐢𝐧𝐤 𝐎𝐟 𝐘𝐨𝐮𝐫 𝐃𝐨𝐜𝐮𝐦𝐞𝐧𝐭 𝐁𝐚𝐛𝐲....")
         path = reply.download()
         fk = upload_file(path)
         for x in fk:
